5ae3394fc9b55c98315027637a493ef4c208af9b
Jon Sherrard <jon@andthats.it>
2018-01-22 12:20:47 +0000

    add email transport

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Hapi from 'hapi'
+import nodemailer from 'nodemailer'
+
+const emailConfig = {
+  service: 'Mailgun',
+  auth: {
+    user: process.env.MAILGUN_USERNAME,
+    pass: process.env.MAILGUN_PASSWORD
+  }
+}
+
+const emailTransport = mailer.createTransport(emailConfig)
 
 const server = new Hapi.Server({
   port: process.env.POST || 3000
