4a1e4e194fb113acd9a7e9485199b3649b9486a4
Jon Sherrard <jon@andthats.it>
2018-01-22 14:39:59 +0000

    start setting up send email functionality

--- src/send-email.js
+++ src/send-email.js
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer'
+import Liquid from 'liquid-node'
 
+const engine = new Liquid.Engine
 const emailConfig = {
   service: 'Mailgun',
   auth: {
@@ -10,6 +12,17 @@ const emailConfig = {
 
 const emailTransport = nodemailer.createTransport(emailConfig)
 
-export default (request, h) => {
-  h.response('testing')
+export default async (request, h) => {
+  const htmlString = await engine.parseAndRender(
+    global.templateString,
+    request.payload
+  )
+  const mailgunResponse = await emailTransport.sendMail({
+    from: process.env.MAILGUN_FROM_ADDRESS,
+    to: request.payload.emailAddress,
+    subject: request.payload.emailSubject,
+    html: htmlString
+  })
+  return h.response(request.payload)
+    .code(200)
 }
