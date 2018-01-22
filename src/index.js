a2b2da49b78a855ac25195a6a8ccfd5e0bc80b7f
Jon Sherrard <jon@andthats.it>
2018-01-22 13:02:24 +0000

    Download template from remote

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+// Load .env variables onto process.env
+require('dotenv').config()
+
 import Hapi from 'hapi'
 import sendEmail from './send-email'
-import request from 'request'
+import request from 'request-promise-native'
+
+// Store template string on a global
+global.templateString = ''
+
 
 const server = new Hapi.Server({
   port: process.env.POST || 3000
@@ -16,6 +23,15 @@ server.route({
 })
 
 server.route({
+  method: 'GET',
+  path: '/template',
+  handler: (request, h) => {
+    return h.response(global.templateString)
+      .code(200)
+  }
+})
+
+server.route({
   method: 'POST',
   path: '/send',
   handler: sendEmail
@@ -23,11 +39,10 @@ server.route({
 
 async function boot() {
   // Download Liquid Template
-  await request(process.env.LIQUID_TEMPLATE_URL)
+  let response = await request(process.env.LIQUID_TEMPLATE_URL)
+  global.templateString = response
   await server.start()
   console.log('Server started at: ' + server.info.uri)
 }
 
 boot()
-
-
